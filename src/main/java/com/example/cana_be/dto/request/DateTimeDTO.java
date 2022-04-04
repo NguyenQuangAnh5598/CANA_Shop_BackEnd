11ff8d0beaaa8b7aa9ff33b852d0ae22b2c05c3e
NguyenQuangAnh5598 <quanganh050598@gmail.com>
2022-04-04 11:13:01 +0700

    #14-created DateTimeDTO

---
+++ src/main/java/com/example/cana_be/dto/request/DateTimeDTO.java
@@ -0,0 +1,31 @@
+package com.example.cana_be.dto.request;
+
+
+import java.util.Date;
+
+public class DateTimeDTO {
+    private Date startDate;
+    private Date endDate;
+
+
+    public DateTimeDTO(Date startDate, Date endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
